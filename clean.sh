fde87c34637ff85b02f2c96ade74f79f1a638e9b
karita <shigekikarita@gmail.com>
2018-12-01 15:24:11 +0900

    auto deploy

---
+++ clean.sh
@@ -0,0 +1,6 @@
+# html
+rm -fv **/*.html **/*.html\~
+
+# latex
+rm -fv **/*.aux **/*.tex **/*.dvi **/*.bbl **/*.blg **/bib2html* **/*.pdf **/*.out **/*.log **/*.toc
+
---
+++ install.el
@@ -0,0 +1,28 @@
+#!emacs --script
+
+(require 'package)
+
+;; MELPA
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Org
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+(package-initialize)
+(package-refresh-contents)
+
+(defvar my/favorite-packages
+  '(
+    d-mode
+    cuda-mode
+    lua-mode
+    yaml-mode
+
+    htmlize
+    spacemacs-theme
+    ))
+
+;; my/favorite-packages
+(dolist (package my/favorite-packages)
+  (unless (package-installed-p package)
+    (package-install package)))
+
