6558d60e4d52be69fe6fe0ce0151b75c5133b05c
karita <shigekikarita@gmail.com>
2018-12-01 16:34:25 +0900

    fix travis

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 # html
-rm -fv **/*.html **/*.html\~
+rm -fv *.html *.html\~ **/*.html **/*.html\~
 
 # latex
 rm -fv **/*.aux **/*.tex **/*.dvi **/*.bbl **/*.blg **/bib2html* **/*.pdf **/*.out **/*.log **/*.toc
